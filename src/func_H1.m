3b29fb0d5f63d09d96a055fc6d1e3c20e40fccf3
Yi Jiang <yijianglanl@users.noreply.github.com>
2025-10-31 13:34:00 -0400

    Delete src directory

--- src/func_H1.m
+++
@@ -1,5 +0,0 @@
-function z = func_H1(rsq,esq)
-
-z = (2*esq + rsq)./(esq + rsq).^(3/2); 
-z = z/(8*pi); 
-
--- src/func_H2.m
+++
@@ -1,4 +0,0 @@
-function z=func_H2(rsq,esq)
-z =  1./(esq + rsq).^(3/2); 
-z = z/(8*pi); 
-
